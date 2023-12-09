9cbd3b91d4a3a4c87ad279946f253bf7e27175f4
cristian-simba <davidsimba200@gmail.com>
2023-12-09 18:18:41 -0500

    promediar

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -21,4 +21,9 @@ public class Calculadora {
 		resultado = valor1 / valor2;
 		return resultado;
 	}
+	public double promediar(double valor1, double valor2, double valor3) {
+		double resultado;
+		resultado = (valor1 + valor2 + valor3)/3;
+		return resultado;
+	}
 }
