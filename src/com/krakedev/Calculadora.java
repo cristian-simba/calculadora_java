7f87aaa83b80ae1c4312d40e240bd5acabfabacf
cristian-simba <davidsimba200@gmail.com>
2023-12-09 18:18:00 -0500

    multiplicar

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -1,5 +1,19 @@
 package com.krakedev;
 
 public class Calculadora {
-
+	public double sumar(double valor1, double valor2) {
+		double resultado;
+		resultado = valor1 + valor2;
+		return resultado;
+	}
+	public double restar(double valor1, double valor2) {
+		double resultado;
+		resultado = valor1 - valor2;
+		return resultado;
+	}
+	public double multiplicar(double valor1, double valor2) {
+		double resultado;
+		resultado = valor1 * valor2;
+		return resultado;
+	}
 }
